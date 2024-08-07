0b16f314a16338ba455756503f6ab23a6110e527
minyang3194 <50267545+minyang3194@users.noreply.github.com>
2024-08-08 04:46:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,26 @@ sed -i 's/192.168.1.1/192.168.2.99/g' package/base-files/files/bin/config_genera
 #sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
 #sed -i "s/.*PKG_VERSION:=.*/PKG_VERSION:=4.3.9_v1.2.14/" package/lean/qBittorrent-static/Makefile
 # welcome test 
+
+#添加istore
+git clone --depth=1 https://github.com/linkease/istore-ui.git
+cp -rf istore-ui/app-store-ui package/app-store-ui
+git clone --depth=1 https://github.com/linkease/istore.git
+cp -rf istore/luci/luci-app-store package/luci-app-store
+sed -i 's/luci-lib-ipkg/luci-base/g' package/luci-app-store/Makefile
+#rm -rf istore-ui istore
+
+#添加luci-app-amlogic
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic.git
+cp -rf luci-app-amlogic/luci-app-amlogic package/luci-app-amlogic
+#rm -rf luci-app-amlogic
+
+#修改晶晨宝盒默认配置
+# 1.Set the download repository of the OpenWrt files to your github.com
+sed -i "s|https.*/OpenWrt|https://github.com/HoldOnBro/Actions-OpenWrt|g" package/luci-app-amlogic/root/etc/config/amlogic
+# 2.Set the keywords of Tags in your github.com Releases
+#sed -i "s|ARMv8|ARMv8|g" package/luci-app-amlogic/root/etc/config/amlogic
+# 3.Set the suffix of the OPENWRT files in your github.com Releases
+sed -i "s|.img.gz|_FOL+SFE.img.gz|g" package/luci-app-amlogic/root/etc/config/amlogic
+# 4.Set the download path of the kernel in your github.com repository
+sed -i "s|opt/kernel|BuildARMv8|g" package/luci-app-amlogic/root/etc/config/amlogic
